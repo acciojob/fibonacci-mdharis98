0868aca94469fb0fa1ed0d172c553a0906cdc0ef
Mohammad Haris <112751385+mdharis98@users.noreply.github.com>
2024-09-19 14:19:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function fibonacci(num) {
     if (num === 0) return 0; // Base case for num = 0
-    if (num === 1) return 0; // Base case for num = 1 (since 0 is the first term)
+    if (num === 1) return 1; // Base case for num = 1
 
     let a = 0, b = 1;
     
@@ -12,5 +12,4 @@ function fibonacci(num) {
     
     return b;
 }
-
 module.exports = fibonacci;
