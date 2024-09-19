d07ad3bfae697ab3329a0cf49d06c3fbc697ef7a
Mohammad Haris <112751385+mdharis98@users.noreply.github.com>
2024-09-19 14:16:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function fibonacci(num) {
-    if (num === 0) return 0;
-    if (num === 1) return 1;
+    if (num === 0) return 0; // Base case for num = 0
+    if (num === 1) return 0; // Base case for num = 1 (since 0 is the first term)
 
     let a = 0, b = 1;
     
