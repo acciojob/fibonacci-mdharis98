5fd1641aa94dff22a1288f84021a4a3c65e2e248
Mohammad Haris <112751385+mdharis98@users.noreply.github.com>
2024-09-19 14:10:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 function fibonacci(num) {
-// your code here
+    if (num === 0) return 0;
+    if (num === 1) return 1;
+
+    let a = 0, b = 1;
+    
+    for (let i = 2; i <= num; i++) {
+        let next = a + b;
+        a = b;
+        b = next;
+    }
+    
+    return b;
 }
 
 module.exports = fibonacci;
